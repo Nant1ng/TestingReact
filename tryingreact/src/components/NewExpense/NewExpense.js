b6a393fd011f572a335d898d8ff5440f1e249689
Nant1ng <andressantana99@hotmail.se>
2021-04-18 14:59:38 +0200

    New Expenses

--- tryingreact/src/components/NewExpense/NewExpense.js
+++ tryingreact/src/components/NewExpense/NewExpense.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import './NewExpense.css';
+import "./ExpenseForm";
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
 
 function NewExpense() {
-    return <div className="new-expense">
-        
+  return (
+    <div className="new-expense">
+      <ExpenseForm />
     </div>
-};
+  );
+}
 
 export default NewExpense;
